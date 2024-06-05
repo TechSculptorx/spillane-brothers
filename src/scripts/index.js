531c07d5f0bbbddbf578c1cebd0f06fe371ae452
Priyanshu <priyanshuvanra2@gmail.com>
2024-06-05 22:13:26 +0530

    Add about page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
 import "../styles/index.scss";
 
+// Navigation hamburger
+const navBtn = document.querySelector(".nav-btn");
+const navLinks = document.querySelector(".nav-links");
+
+navBtn.addEventListener("click", () => {
+  navBtn.classList.toggle("open");
+
+  setTimeout(() => {
+    navLinks.classList.toggle("open");
+  }, 200);
+});
+
 console.log("Hello, world!");
