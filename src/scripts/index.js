4df610d8cf011ceded20a5b1ba71f3143d1d2ef3
Priyanshu <priyanshuvanra2@gmail.com>
2024-06-10 11:29:04 +0530

    add project page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,4 +12,53 @@ navBtn.addEventListener("click", () => {
   }, 200);
 });
 
+// Back to top
+const backToTop = document.querySelector(".back-to-top");
+
+if (backToTop) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+      backToTop.classList.add("show");
+    } else {
+      backToTop.classList.remove("show");
+    }
+  });
+
+  backToTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+// Tabs section
+const tabs = document.querySelectorAll(".tabs");
+
+if (tabs.length > 0) {
+  tabs.forEach((tab) => {
+    const tabsSwitches = tab.querySelectorAll(".tab-switch");
+    const tabsContents = tab.querySelector(".tabs-content");
+
+    tabsSwitches.forEach((tabSwitch) => {
+      tabSwitch.addEventListener("click", () => {
+        const swichId = tabSwitch.getAttribute("data-tab");
+        const tabCont = tabsContents.querySelector(
+          `[data-content="${swichId}"]`
+        );
+
+        tabsSwitches.forEach((tab) => tab.classList.remove("active"));
+        tabSwitch.classList.add("active");
+
+        tabsContents
+          .querySelectorAll(".tabs-content-detail")
+          .forEach((content) => {
+            content.classList.remove("active");
+          });
+        tabCont.classList.add("active");
+      });
+    });
+  });
+}
+
 console.log("Hello, world!");
